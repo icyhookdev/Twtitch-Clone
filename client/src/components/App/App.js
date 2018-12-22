41855ca99e7ac0d72c425d208c0a2cf39b912afd
thienJs <cristian.albarran95@gmail.com>
2018-12-21 21:42:58 -0400

    Header nav component created

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import StreamCreate from './stream/StreamCreate/StreamCreate';
-import StreamDelete from './stream/StreamDelete/StreamDelete';
-import StreamEdit from './stream/StreamEdit/StreamEdit';
-import StreamList from './stream/StreamList/StreamList';
-import StreamShow from './stream/StreamShow/StreamShow';
+import StreamCreate from '../stream/StreamCreate/StreamCreate';
+import StreamDelete from '../stream/StreamDelete/StreamDelete';
+import StreamEdit from '../stream/StreamEdit/StreamEdit';
+import StreamList from '../stream/StreamList/StreamList';
+import StreamShow from '../stream/StreamShow/StreamShow';
+import Header from '../Header/Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
